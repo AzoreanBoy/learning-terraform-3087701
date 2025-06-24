45d1cdfe52999cfe6fc565215c433538adb55475
Pedro Ferreira <102299401+AzoreanBoy@users.noreply.github.com>
2025-06-24 11:40:00 +0100

    ALB Version Atleration

--- main.tf
+++ main.tf
@@ -46,17 +46,15 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "9.17.0"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "7.0.0"
 
-  name    = "blog-alb"
+  name = "blog-alb"
 
-  load_balancer_type = "application"
-
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {

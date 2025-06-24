cab2e95be638ce4dd00884f13d165e419c3f8fe5
Pedro Ferreira <102299401+AzoreanBoy@users.noreply.github.com>
2025-06-24 16:51:38 +0100

    Revert to Last Applied

--- main.tf
+++ main.tf
@@ -33,20 +33,17 @@ module "blog_vpc" {
   }
 }
 
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.3.0"
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  name = "blog"
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
-    
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 module "blog_alb" {
